e9289d2a211c8f89395f9e104473e811eb1e481e
Moshe <mosheyaku@gmail.com>
2023-06-17 23:44:33 +0300

    Adding documentation.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,23 @@
 import java.util.Random;
 import java.util.Scanner;
 
+/* The Main class serves as the entry point for the program and coordinates
+the matrix multiplication process.
+It provides methods for matrix generation, display, and thread management.
+*/
 public class Main {
 
     private static final int UPPER_BOUND_OF_MATRIX_VALUES = 11;
 
+    /*The main method of the program.*/
     public static void main(String[] args) {
         boolean isValidMatrixSizes = false;
         int n = 0, m = 0, p = 0;
+        //Prompt the user to enter valid matrix sizes until valid sizes are provided.
         while (!isValidMatrixSizes) {
             try {
                 Scanner scanner = new Scanner(System.in);
+                // Get the dimensions of the matrices from the user.
                 n = isValidSize(scanner, 'n');
                 m = isValidSize(scanner, 'm');
                 p = isValidSize(scanner, 'p');
@@ -20,30 +27,33 @@ public class Main {
                 System.out.println("Error!\nPlease choose positive numbers.\n");
             }
         }
-
+        // Create the matrices
         int[][] matA = new int[n][m];
         int[][] matB = new int[m][p];
 
+        // Generate random values for the matrices.
         Random rand = new Random();
-
         setMatrix(matA, rand, n, m);
-
         setMatrix(matB, rand, m, p);
 
+        // Display the matrices
         displayMatrix(matA, 'A');
-
         displayMatrix(matB, 'B');
 
-        MyMonitor monitor = new MyMonitor(n, p);
+        // Create monitor and thread arrays.
+        Monitor monitor = new Monitor(n, p);
         MatrixMultiplication t[][] = new MatrixMultiplication[n][p];
 
+        // Create threads for matrix multiplication.
         createThreads(t, matA, matB, monitor, m);
 
+        // Run the matrix multiplication threads
         System.out.println("The multiplication of matrix A and matrix B:");
         runThreads(t);
     }
 
-
+    /*The method receives an input and checks whether it is a positive integer.
+    If so, it returns it, if not, an error will be returned.*/
     public static int isValidSize(Scanner scanner, char sizeName) {
         int size = 0;
         System.out.printf("Please choose %c: ", sizeName);
@@ -53,6 +63,7 @@ public class Main {
         return size;
     }
 
+    /*The method gets a matrix and fills it with random integer values.*/
     public static void setMatrix(int[][] matrix, Random rand, int row, int col) {
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
@@ -61,6 +72,7 @@ public class Main {
         }
     }
 
+    /*This method displays the elements of the given matrix.*/
     public static void displayMatrix(int[][] matrix, char matrixName) {
         System.out.println("Matrix " + matrixName);
         for (int i = 0; i < matrix.length; i++) {
@@ -72,7 +84,9 @@ public class Main {
         System.out.println();
     }
 
-    public static void createThreads(MatrixMultiplication t[][], int[][] matA, int[][] matB, MyMonitor monitor, int m) {
+    /*This method creates threads for matrix multiplication
+    and assigns them to the provided thread array.*/
+    public static void createThreads(MatrixMultiplication t[][], int[][] matA, int[][] matB, Monitor monitor, int m) {
         for (int i = 0; i < t.length; i++) {
             for (int j = 0; j < t[0].length; j++) {
                 t[i][j] = new MatrixMultiplication(matA, matB, i, j, m, monitor);
@@ -80,6 +94,7 @@ public class Main {
         }
     }
 
+    /*This method starts the matrix multiplication threads.*/
     public static void runThreads(MatrixMultiplication t[][]) {
         for (int i = 0; i < t.length; i++) {
             for (int j = 0; j < t[0].length; j++) {
